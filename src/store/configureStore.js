c7f93e3813318fccc2ef1bd15fef7582da80a107
max <maxim.maximov@netcracker.com>
2019-10-23 19:56:58 +0300

    update

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,11 +4,15 @@ import {applyMiddleware, createStore} from "redux";
 import monitorReducerEnhancer from "./enhancers/monitorReducer";
 import {composeWithDevTools} from "redux-devtools-extension";
 import reducers from "./reducers";
+import {loadState, saveState} from "./localstorage";
+import {throttle} from "lodash";
 
 // для того чтобы кучу логики не хранить в index.js про стор, вынес в отдельный файл
 
 export default function configureStore() {
     const initState = {};
+    // забираю стор из локалстораджа
+    const persistedState = loadState();
 
     const middlewares = [thunk, logger];
     const middlewareEnhancer = applyMiddleware(...middlewares);
@@ -21,10 +25,18 @@ export default function configureStore() {
 
     const store = createStore(
         reducers,
-        initState,
+        persistedState || initState,
         // должен все engancers объединять в один, так как createStore может принимать только 1 аргумент enhancers
         composedEnhancers,
     );
+
+    // throttle чтобы часто не вызывать дорогую JSON.parse хотябы 1 раз в секунду
+    store.subscribe(throttle(() => {
+        saveState({
+            // сохраняю только дату которую получил с бе но не сохраняю вью дату
+            songs: store.getState().songs
+        });
+    }, 1000));
 
     return store
 }
