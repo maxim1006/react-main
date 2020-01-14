202d6c8f1b2d654db7634089306393a3504aec46
max <maximprosv@gmail.com>
2020-01-14 23:21:56 +0300

    update

--- src/components/theme/ThemeSelector.js
+++ src/components/theme/ThemeSelector.js
@@ -2,10 +2,10 @@ import React, {useContext} from "react";
 import ThemeContext from "../../context/ThemeContext";
 
 export default () => {
-    const {value, onThemeChange} = useContext(ThemeContext);
+    const {theme, onThemeChange} = useContext(ThemeContext);
 
     return (
-        <select value={value} onChange={onThemeChange}>
+        <select value={theme} onChange={onThemeChange}>
             <option value="default">default</option>
             <option value="theme1">theme1</option>
             <option value="theme2">theme2</option>
