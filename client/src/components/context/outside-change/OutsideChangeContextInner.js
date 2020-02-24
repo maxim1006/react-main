a24912e68c9399430f1933939d96151fdbc2df20
max <maximprosv@gmail.com>
2020-02-24 15:19:30 +0300

    update

---
+++ client/src/components/context/outside-change/OutsideChangeContextInner.js
@@ -0,0 +1,19 @@
+import React, {memo, useContext} from "react";
+import OutsideChangeContext from "../../../context/OutsideChangeContext";
+
+export default memo(() => {
+    const {setValue, value} = useContext(OutsideChangeContext);
+
+    return (
+        <>
+            Context value: {JSON.stringify(value)};
+
+            <select name="outsideContextSelect" onChange={setValue}>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+            </select>
+        </>
+    );
+
+});
--- client/src/pages/ContextPage.js
+++ client/src/pages/ContextPage.js
@@ -1,4 +1,18 @@
 import React, {memo} from "react";
 import ContextTest from "../components/context/test/ContextTest";
+import Component from "../components/component/Component";
+import OutsideChangeContextWrapper from "../components/context/outside-change/OutsideChangeContextWrapper";
 
-export default memo(() => <ContextTest/>);
+export default memo(() => {
+    return (
+        <>
+            <Component title="OusideChange">
+                <OutsideChangeContextWrapper />
+            </Component>
+
+            <Component title="Language example">
+                <ContextTest/>
+            </Component>
+        </>
+    );
+});
