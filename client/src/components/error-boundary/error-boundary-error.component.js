58f61497d2789f3600ad7d2d6bca80f7ef137346
max <maximprosv@gmail.com>
2020-04-20 12:59:36 +0300

    update

---
+++ client/src/components/error-boundary/error-boundary-error.component.js
@@ -0,0 +1,9 @@
+import React, {PureComponent} from "react";
+
+export class ErrorBoundaryError extends PureComponent {
+    render() {
+        // render обязательно должен что-то возврщать и если бы не было обернуто в ErrorBoundary,
+        // то приложение бы крашнулось, а так только ошибки, которые обрабатываю в ErrorBoundary
+        return undefined;
+    }
+}
--- client/src/pages/ReactPage.js
+++ client/src/pages/ReactPage.js
@@ -6,15 +6,23 @@ import JSXExampleComponent from "../components/jsx/JsxExamples";
 import {TabsComponent} from "../components/tabs/Tabs";
 import ReduxForm from "../components/forms/ReduxForm";
 import Component from "../components/component/Component";
-import NavBar from "../components/navbar/navbar.component";
+import {ErrorBoundary} from "../components/error-boundary/error-boundary.component";
+import {ErrorBoundaryError} from "../components/error-boundary/error-boundary-error.component";
+import {LifecycleHooksComponent} from "../components/lifecycle-hooks/LifecycleHooks";
 
 export default function ReactPage() {
     return (
         <TabsComponent>
             <div tabName="Components">
-                <Component title="Dropdown Menu">
-                    <NavBar/>
+                <Component title="Error boundary">
+                    <ErrorBoundary>
+                        <ErrorBoundaryError/>
+                    </ErrorBoundary>
                 </Component>
+
+                {/*<Component title="Dropdown Menu">*/}
+                {/*    <NavBar/>*/}
+                {/*</Component>*/}
 
                 {/*<Component title="Insert HTML">*/}
                 {/*    <DangerouslySetInnerHTML/>*/}
