44d7f7391635931f67f76544401e66e3522dd3bd
max <maximprosv@gmail.com>
2020-07-17 23:11:49 +0300

    update

--- client/src/common/api/axios.ts
+++ client/src/common/api/axios.ts
@@ -9,3 +9,35 @@ customAxios.CancelToken = axios.CancelToken;
 customAxios.isCancel = axios.isCancel;
 
 export default customAxios;
+
+// пример кастомного error handling
+// const ErrorDescription = ({ error }) => {
+//     return (
+//         <>
+//             Please try again later <br /> {error}
+//     </>
+// );
+// };
+//
+// customAxios.interceptors.response.use(
+//     function (response) {
+//         // Do something with response data
+//         return response;
+//     },
+//     function (error) {
+//         notification['error']({
+//             message: 'Something went wrong',
+//             description: <ErrorDescription error={error.message} />,
+//     });
+//         // Do something with response error
+//         return Promise.reject(error);
+//     }
+// );
+// также с request
+// axios.interceptors.request.use(function (config) {
+//     // Do something before request is sent
+//     return config;
+// }, function (error) {
+//     // Do something with request error
+//     return Promise.reject(error);
+// });
