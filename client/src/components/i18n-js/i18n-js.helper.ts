be043f0a882e1d48bb7cac5ffbf8dbd23082804b
max <maximprosv@gmail.com>
2020-07-19 15:31:06 +0300

    update

--- client/src/components/i18n-js/i18n-js.helper.ts
+++ client/src/components/i18n-js/i18n-js.helper.ts
@@ -1,22 +1,27 @@
 import i18n from "i18n-js";
-import customAxios from "../../common/api/axios";
 
-import translations from "../../i18n/translations/translations";
+import en from "../../i18n/translations/locales/en.json";
 
 i18n.fallbacks = true;
-i18n.translations = translations;
+// i18n.translations = translations;
+i18n.translations = { en, ru: {} };
 i18n.defaultLocale = "en";
 
 export default i18n;
 
 export const loadTranslations = async () => {
     await Promise.all(
-        Object.keys(i18n.translations).map(async locale => {
-            const { data } = await customAxios.get(`/translations/${locale}.json`);
-            i18n.translations[locale] = {
-                ...i18n.translations[locale],
-                ...data
-            };
-        })
+        Object.keys(i18n.translations)
+            .filter(key => key !== i18n.defaultLocale)
+            .map(async locale => {
+                const { default: data } = await import(`../../i18n/translations/locales/${locale}.json`);
+                console.log(data);
+                i18n.translations[locale] = {
+                    ...i18n.translations[locale],
+                    ...data
+                };
+            })
     );
 };
+
+loadTranslations();
