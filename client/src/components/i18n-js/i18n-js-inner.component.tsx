233f4ff835f25a7313fc62745b0d3800debf4953
max <maximprosv@gmail.com>
2020-07-21 10:52:33 +0300

    update

---
+++ client/src/components/i18n-js/i18n-js-inner.component.tsx
@@ -0,0 +1,21 @@
+import React, { memo, useContext } from "react";
+import i18nContext from "../../context/i18nContext";
+
+// https://github.com/fnando/i18n-js
+
+const I18nJsInnerComponent: React.FC = () => {
+    const { i18n } = useContext(i18nContext);
+
+    return (
+        <div>
+            <h3>I18n Inner</h3>
+            <p>{i18n.t("browse")}</p>
+            <p>{i18n.t("browse", { locale: "ru" })}</p>
+            <p>{i18n.t("complex.prop", { count: 10 })}</p>
+            <p>{i18n.t("cancel")}</p>
+            <p>{i18n.t("clear")}</p>
+        </div>
+    );
+};
+
+export default memo(I18nJsInnerComponent);
--- client/src/components/i18n-js/i18n-js.component.tsx
+++ client/src/components/i18n-js/i18n-js.component.tsx
@@ -1,15 +1,22 @@
 import React, { memo, useState } from "react";
 import i18n from "./i18n-js.helper";
+import I18nJsInnerComponent from "./i18n-js-inner.component";
+import i18nContext from "../../context/i18nContext";
 
 // https://github.com/fnando/i18n-js
-
+// Сделал пример с иннер так как компоненты с мемо не будут обновляться при смене локали надоо использовать контекст
 const I18nJsComponent: React.FC = () => {
     const [locale, setLocale] = useState(i18n.currentLocale());
+
+    const setValue = (value: any) => {
+        i18n.locale = value;
+        setLocale(value);
+    };
 
     i18n.locale = locale;
 
     return (
-        <>
+        <i18nContext.Provider value={{ i18n, setValue }}>
             <div>
                 <button
                     type="button"
@@ -25,7 +32,9 @@ const I18nJsComponent: React.FC = () => {
             <p>{i18n.t("complex.prop", { count: 10 })}</p>
             <p>{i18n.t("cancel")}</p>
             <p>{i18n.t("clear")}</p>
-        </>
+
+            <I18nJsInnerComponent />
+        </i18nContext.Provider>
     );
 };
 
