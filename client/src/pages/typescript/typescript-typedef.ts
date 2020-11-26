681a444ae93481d17e2cbbe3581a008a55033c62
Max Maximov <maxim.maximov@netcracker.com>
2020-11-26 23:35:23 +0300

    update

---
+++ client/src/pages/typescript/typescript-typedef.ts
@@ -0,0 +1,31 @@
+interface FamilyMemberNameModel {
+    lang: string;
+    value: string;
+}
+
+function addFamilyMember(name: string | FamilyMemberNameModel) {
+    // а с проверкой на typedef все норм
+    if (isFamilyMemberNameModel(name)) {
+        console.log(name.lang);
+    }
+
+    // так будет ошибка
+    // console.log(name.lang);
+}
+
+export default function isFamilyMemberNameModel(name: string | FamilyMemberNameModel): name is FamilyMemberNameModel {
+    return (name as FamilyMemberNameModel).lang !== undefined;
+}
+
+// Для того чтобы проверить на собственные созданные интерфейсы использую технику создания typedef функции
+// и теперь могу что угодно проверять
+// type ProductOfferingRowProps = {
+//     model: RadioButtonModel | string;
+//     translateKey: string;
+// };
+//
+// function isRadioButtonModel(model: RadioButtonModel | string): model is RadioButtonModel {
+//     return (model as RadioButtonModel).items !== undefined;
+// }
+//
+// <div className={styles.productRowContent}>{isRadioButtonModel(model) && <RadioButton model={model} />}</div>
