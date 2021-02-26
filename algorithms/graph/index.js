bf0e260869ec2d0f960c2428248801c5634460bc
Max Maximov <maxim.maximov@netcracker.com>
2021-02-26 10:07:56 +0300

    update

---
+++ algorithms/graph/index.js
@@ -0,0 +1,116 @@
+// Part1 Represent graph, undirected, not weighted, no cycles
+const airports = 'PHX BKK OKC JFK LAX MEX EZE HEL LOS LAP LIM'.split(' ');
+
+const routes = [
+    ['PHX', 'LAX'],
+    ['PHX', 'JFK'],
+    ['JFK', 'OKC'],
+    ['JFK', 'HEL'],
+    ['JFK', 'LOS'],
+    ['MEX', 'LAX'],
+    ['MEX', 'BKK'],
+    ['MEX', 'LIM'],
+    ['MEX', 'EZE'],
+    ['LIM', 'BKK'],
+];
+
+// the graph adjacency
+const adjacencyList = new Map();
+
+// first add node to graph
+function addNode(airport) {
+    adjacencyList.set(airport, []);
+}
+
+// Add edge, undirected
+function addEdge(origin, destination) {
+    adjacencyList.get(origin).push(destination);
+    adjacencyList.get(destination).push(origin);
+}
+
+// create the graph
+airports.forEach(addNode);
+routes.forEach(route => addEdge(...route));
+
+// console.log({ adjacencyList });
+// {
+//     'PHX' => [ 'LAX', 'JFK' ],
+//     'BKK' => [ 'MEX', 'LIM' ],
+//     'OKC' => [ 'JFK' ],
+//     'JFK' => [ 'PHX', 'OKC', 'HEL', 'LOS' ],
+//     'LAX' => [ 'PHX', 'MEX' ],
+//     'MEX' => [ 'LAX', 'BKK', 'LIM', 'EZE' ],
+//     'EZE' => [ 'MEX' ],
+//     'HEL' => [ 'JFK' ],
+//     'LOS' => [ 'JFK' ],
+//     'LAP' => [],
+//     'LIM' => [ 'MEX', 'BKK' ]
+// }
+
+// Part 2 Graph Search or Traversal, if there is a route between PHX and BKK
+// can do with DFS, BFS,
+//
+// do with dfs
+// const BKK = 'BKK';
+// function dfs(start, visited = new Set()) {
+//     visited.add(start);
+//
+//     let canFly = false;
+//     let nodes = adjacencyList.get(start);
+//
+//     if (!nodes || nodes.length === 0) {
+//         return false;
+//     }
+//
+//     console.log({ start, nodes, visited });
+//
+//     for (let key of nodes) {
+//         if (key === BKK) {
+//             canFly = true;
+//             return true;
+//         }
+//
+//         if (!visited.has(key)) {
+//             canFly = dfs(key, visited);
+//
+//             if (canFly) return canFly;
+//         }
+//     }
+//
+//     return canFly;
+// }
+//
+// console.log(dfs('PHX'));
+
+// do with bfs
+// const BKK = 'BKK';
+// function bfs(start, visited = new Set()) {
+//     let canFly = false;
+//
+//     const queue = [start];
+//
+//     while (queue.length) {
+//         const airport = queue.shift();
+//         let nodes = adjacencyList.get(airport);
+//         visited.add(airport);
+//
+//         for (const node of nodes) {
+//             if (node === BKK) {
+//                 canFly = true;
+//                 return canFly;
+//             }
+//
+//             if (!visited.has(node)) {
+//                 queue.push(node);
+//             }
+//         }
+//
+//         console.log({ start, queue, visited });
+//     }
+//
+//     return canFly;
+// }
+//
+// console.log(bfs('PHX'));
+
+// Time complexity for the algorithms bfs, dfs is O(vertexes + edges) (big O notation), therefore the time is O(n), linear
--- algorithms/graph/readme.md
+++ algorithms/graph/readme.md
@@ -4,4 +4,13 @@ we can go anywhere because there are no directions
 ### directed
 Has directions where we can go from one node to another
 
-Graphs have vertexes (nodes) with some type of value associated. Between vertexes there are edges (connector line) which also have value
+Graphs have vertexes (nodes) with some type of value associated. Between vertexes there are edges (connector line) which also have value/weight (weighted graph)
+
+Can  solve with 
+
+**adjacency matrix**, add 1 row and 1 col for each node and add 1 in a cell where they intersect, drawback - quadratic complexity and time to insert anything
+
+**adjacency list** - each item has its own array of neighbours, faster to iterate and more efficient in memory
+
+To traverse graph can use DFS (depth first search) or BDF (breadth-first-search)
+
