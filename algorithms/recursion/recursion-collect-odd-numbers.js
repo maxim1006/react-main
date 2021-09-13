9f941f7b3190e06838397ab4e87bf3105a596cf3
Max Maximov <maxim.maximov@netcracker.com>
2021-09-13 09:55:28 +0300

    update

--- algorithms/recursion/recursion-collect-odd-numbers.js
+++ algorithms/recursion/recursion-collect-odd-numbers.js
@@ -16,7 +16,19 @@ function collectOdd(nums) {
     return result;
 }
 
-// pure recursion
+// pure recursion - это чисто для примера чтобы понять что такое рекурсия
+// function collectOddPure(nums) {
+//     let arr = [];
+//
+//     if (!nums.length) return [];
+//
+//     if (nums[0] % 2 !== 0) arr.push(nums[0]);
+//
+//     arr = [...arr, ...collectOddPure(nums.slice(1))];
+//
+//     return arr;
+// }
+
 function collectOddPure(nums) {
     let arr = [];
 
@@ -24,9 +36,7 @@ function collectOddPure(nums) {
 
     if (nums[0] % 2 !== 0) arr.push(nums[0]);
 
-    arr = [...arr, ...collectOddPure(nums.slice(1))];
-
-    return arr;
+    return [...arr, ...collectOddPure(nums.slice(1))];
 }
 
 console.log(collectOdd([1, 2, 3, 4, 5, 6, 7, 8, 9]));
