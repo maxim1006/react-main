b552d7c52f7687fbc4b5a1ced44d875e892b441f
Max Maximov <maxim.maximov@netcracker.com>
2022-01-13 10:54:47 +0300

    update

--- packages/projects/simple/webpack.config.js
+++ packages/projects/simple/webpack.config.js
@@ -1,37 +1,58 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+const mode = process.env.NODE_ENV;
+const isProd = mode === "production";
 
 module.exports = {
+    entry: './src/index.ts',
+    mode,
     output: {
-        publicPath: 'http://localhost:8081/'
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist'),
     },
+
+    devtool: isProd ? false : 'inline-source-map',
 
     resolve: {
         extensions: ['.tsx', '.ts', '.jsx', '.js', '.json']
     },
 
     devServer: {
-        port: 8081
+        port: 8081,
+        compress: true,
     },
 
     module: {
         rules: [
             {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader']
+                test: /\.(sa|sc|c)ss$/i,
+                use: [ isProd ? MiniCssExtractPlugin.loader: "style-loader", 'css-loader', "postcss-loader", "sass-loader",]
             },
             {
                 test: /\.(ts|tsx|js|jsx)$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: 'babel-loader'
+                    loader: 'ts-loader'
                 }
-            }
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
         ]
     },
 
     plugins: [
         new HtmlWebPackPlugin({
             template: './src/index.html'
-        })
+        }),
+        ...[].concat(isProd ? [new MiniCssExtractPlugin({
+            filename: "[name].css",
+        })]: []),
     ]
 };
