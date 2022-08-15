918724e430da58699052eea62b13751ce709e681
Max Maximov <maxim.maximov@netcracker.com>
2022-08-15 13:05:03 +0300

    update

--- client/.stylelintrc.js
+++ client/.stylelintrc.js
@@ -10,6 +10,7 @@ module.exports = {
             },
         ],
         'selector-class-pattern': ['.*'],
+        'custom-property-pattern': ['.*'],
         'color-function-notation': ['legacy'],
         'selector-pseudo-class-no-unknown': [
             true,
