30e3f6c63ab0d1c852404258bfabcbe88250caa8
maxim1006 <maximprosv@gmail.com>
2022-11-12 21:58:50 +0300

    feat: TICKET-00000 added clock game

--- packages/telegram-bot/src/utils/dates.utils.ts
+++ packages/telegram-bot/src/utils/dates.utils.ts
@@ -1,7 +1,7 @@
 import { UserModel } from '../models/user.model';
 
 export const areDatesInSameDay = (date: Date, date1: Date) => {
-    // date.toDateString() // Sun Oct 30 2022
+    // new Date().toDateString() // Sun Oct 30 2022
     return date.toDateString() === date1.toDateString();
 };
 
