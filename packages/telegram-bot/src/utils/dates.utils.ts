8508e771601cba96c0b640c0760a2230605a8173
maxim1006 <maximprosv@gmail.com>
2022-11-18 23:37:18 +0300

    feat: TICKET-00000 added postgres-pg-node

--- packages/telegram-bot/src/utils/dates.utils.ts
+++ packages/telegram-bot/src/utils/dates.utils.ts
@@ -2,7 +2,11 @@ import { UserModel } from '../models/user.model';
 
 export const areDatesInSameDay = (date: Date, date1: Date) => {
     // new Date().toDateString() // Sun Oct 30 2022
-    return date.toDateString() === date1.toDateString();
+    return (
+        date.getMonth() === date1.getMonth() &&
+        date.getDate() === date1.getDate() &&
+        date.getFullYear() === date1.getFullYear()
+    );
 };
 
 export const getTodayDateByUserDataDates = (userData: UserModel) =>
