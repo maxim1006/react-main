f58250b070300683434a67f9880895248d182a70
maxim1006 <maximprosv@gmail.com>
2023-02-06 20:02:29 +0300

    feat: TICKET-00000 added user specific dictionaries

--- packages/telegram-bot/src/utils/user.utils.ts
+++ packages/telegram-bot/src/utils/user.utils.ts
@@ -1,10 +1,10 @@
 import { ENGLISH_DICTIONARIES } from '../constants/english.constants';
 
 export function getUserEnglishDictionary(userName: string = '') {
-    switch (userName) {
+    switch (userName.toLowerCase()) {
         case 'maximprosv':
             return ENGLISH_DICTIONARIES.medium;
-        case 'Fleur_lionceau':
+        case 'fleur_lionceau':
             return ENGLISH_DICTIONARIES.expert;
         default:
             return ENGLISH_DICTIONARIES.easy;
