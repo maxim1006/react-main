0f02b8dc62ece19e71f95ed2a9579e4d9b81ede7
maxim1006 <maximprosv@gmail.com>
2023-05-26 18:02:06 +0300

    feat: TICKET-00000 added css variables example

--- client/src/components/css-variable/css-variable.component.tsx
+++ client/src/components/css-variable/css-variable.component.tsx
@@ -1,12 +1,18 @@
-import React, { memo, FC } from 'react';
+import React, { memo, FC, useEffect, useRef } from 'react';
 import styles from './css-variable.module.scss';
 import cn from 'classnames';
 
 type CssVariableProps = {};
 
 const CssVariable: FC<CssVariableProps> = () => {
+    const ref = useRef<HTMLDivElement>(null!);
+
+    useEffect(() => {
+        ref.current.style.setProperty('--my-css-variable-from-css-padding', '10px 10px');
+    }, []);
+
     return (
-        <div className={cn(styles.host, 'taCssVariable')}>
+        <div ref={ref} className={cn(styles.host, 'taCssVariable')}>
             <div className={styles.fromCss}>fromCss</div>
             <div className={styles.toCss} style={{ padding: 'var(--my-css-variable-to-css-padding, 0 0)' }}>
                 ToCss
