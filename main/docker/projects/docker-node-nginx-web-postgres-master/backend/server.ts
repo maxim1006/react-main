762539973e4de7161b0fde3672c2456a12e6d0e8
maxim1006 <maximprosv@gmail.com>
2024-02-22 12:51:24 +0300

    feat: TICKET-00001 added docker ready use example

---
+++ main/docker/projects/docker-node-nginx-web-postgres-master/backend/server.ts
@@ -0,0 +1,49 @@
+import express from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
+import helmet from 'helmet';
+import cors from 'cors';
+import compression from 'compression';
+
+const app = express();
+
+// compress
+function shouldCompress(req: express.Request, res: express.Response) {
+    if (req.headers['x-no-compression']) {
+        // don't compress responses with this request header
+        return false;
+    }
+
+    // fallback to standard filter function
+    return compression.filter(req, res);
+}
+
+app.use(compression({ filter: shouldCompress }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(cors());
+app.use(helmet({}));
+
+app.use((req, res, next) => {
+    res.setHeader(
+        'Permissions-Policy',
+        'camera=(), fullscreen=(self), geolocation=(self), magnetometer=(), microphone=(), midi=(), payment=(), sync-xhr=()'
+    );
+    next();
+});
+
+// serve static
+const root = path.join(__dirname, './build');
+app.use(express.static(root));
+app.get('/api/v1/health/status', (req, res) => {
+    res.send('Node server is ok');
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(root + '/index.html');
+});
+
+app.listen(3000, () => {
+    console.log('BACKEND_ENV_VARIABLE', process.env.BACKEND_ENV_VARIABLE);
+    console.log(`Server is listening on port ${3000}`);
+});
