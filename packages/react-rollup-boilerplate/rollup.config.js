450e38c23518d09e680a636080fb8e1e6bfba0a2
maxim1006 <maximprosv@gmail.com>
2024-02-28 15:22:48 +0300

    feat: TICKET-00001 added rollup lib

--- packages/react-rollup-boilerplate/rollup.config.js
+++ packages/react-rollup-boilerplate/rollup.config.js
@@ -1,60 +1,46 @@
-import { DEFAULT_EXTENSIONS } from '@babel/core';
-import babel from '@rollup/plugin-babel';
-import typescript from 'rollup-plugin-typescript2';
+import typescript from '@rollup/plugin-typescript';
+import postcss from 'rollup-plugin-postcss';
+import terser from '@rollup/plugin-terser';
 import commonjs from '@rollup/plugin-commonjs';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import postcss from 'rollup-plugin-postcss';
-import resolve from '@rollup/plugin-node-resolve';
 import url from '@rollup/plugin-url';
 import svgr from '@svgr/rollup';
-import { terser } from 'rollup-plugin-terser';
-import visualizer from 'rollup-plugin-visualizer';
-import pkg from './package.json';
 
-export default {
-    input: ['src/index.ts'],
-    output: [
-        {
-            dir: 'dist',
-            format: 'es',
-            preserveModules: true,
-            preserveModulesRoot: 'src',
-        },
-    ],
-    plugins: [
-        postcss({
-            plugins: [],
-            minimize: true,
-            extract: false,
-            modules: true,
-            use: ['less'],
-        }),
-        peerDepsExternal(),
-        typescript({
-            typescript: require('typescript'),
-            include: ['*.js+(|x)', '**/*.js+(|x)'],
-            exclude: ['coverage', 'config', 'dist', 'node_modules/**', '*.test.{js+(|x), ts+(|x)}', '**/*.test.{js+(|x), ts+(|x)}'],
-            tsconfig: 'tsconfig.json',
-        }),
-        babel({
-            ...pkg.babel,
-            extensions: [...DEFAULT_EXTENSIONS, '.ts', '.tsx'],
-            babelHelpers: 'runtime',
-            plugins: [
-                [
-                    'babel-plugin-styled-components',
-                    {
-                        minify: true,
-                        transpileTemplateLiterals: false,
-                    },
-                ],
-            ],
-        }),
-        url(),
-        svgr(),
-        resolve(),
-        commonjs(),
-        terser(),
-        visualizer(),
-    ],
-};
+const LIBRARY_PATH = 'src';
+const EXTERNAL_MODULES = ['node_modules', 'react', '@mui/icons-material', '@mui/lab', '@mui/material', 'style-inject'];
+
+export default [
+    {
+        input: `${LIBRARY_PATH}/index.ts`,
+        external: ['react', 'react-dom', 'antd', 'styled-components', 'antd/lib/dropdown'],
+        output: [
+            {
+                dir: 'dist',
+                format: 'es',
+                preserveModules: true,
+                preserveModulesRoot: LIBRARY_PATH,
+            },
+        ],
+        plugins: [
+            postcss({
+                plugins: [],
+                minimize: true,
+                extract: false,
+                autoModules: true,
+                modules: true,
+                use: ['sass'],
+            }),
+            typescript(),
+            peerDepsExternal(),
+            commonjs(),
+            url(),
+            svgr(),
+            terser(),
+        ],
+        // remove node modules from dist
+        // external: (id) => {
+        //     console.log(id);
+        //     return EXTERNAL_MODULES.some((el) => id.includes(el));
+        // },
+    },
+];
--- packages/react-rollup-boilerplate/src/index.ts
+++ packages/react-rollup-boilerplate/src/index.ts
@@ -1,4 +1,2 @@
 export { Button } from './lib/components/Button';
 export type { IButtonProps } from './lib/components/Button';
-export { Test } from './lib/components/Test';
-export type { TestProps } from './lib/components/Test';
