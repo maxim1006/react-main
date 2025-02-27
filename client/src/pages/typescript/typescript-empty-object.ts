306b16669dc520496b6a5bd9f6d8b99afee4400d
maxim1006 <maximprosv@gmail.com>
2025-02-27 12:51:29 +0300

    feat: TICKET-000014 added empty object ts example

---
+++ client/src/pages/typescript/typescript-empty-object.ts
@@ -0,0 +1,16 @@
+// это валидно так как {} это все кроме null и undefined
+const a: {} = 'string';
+
+type Test<T extends {}> = T;
+type Test1<T extends object> = T;
+
+const b: Test<string> = 'string';
+
+/**
+в Test1<string> будет ошибка так как
+<T extends object>
+Более строгое ограничение.
+Исключает примитивные типы.
+Гарантирует, что T будет объектом.
+*/
+const b1: Test1<object> = {};
