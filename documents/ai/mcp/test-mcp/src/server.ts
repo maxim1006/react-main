4cef67df0a38074c56436f1558583e253804adff
maxim1006 <maximprosv@gmail.com>
2025-08-25 14:32:43 +0300

    feat(TICKET-01155): updated mcp example

--- documents/ai/mcp/test-mcp/src/server.ts
+++ documents/ai/mcp/test-mcp/src/server.ts
@@ -1,14 +1,18 @@
 import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
 import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js';
-import { z } from 'zod';
+import { UserSchema } from './models/user.model';
+import { createUser } from './utils/user.utils';
 
 const server = new McpServer({
     name: 'test-mcp',
     version: '1.0.0',
     // что поддерживает этот сервер
     capabilities: {
+        // resources — описание доступных серверу внешних или внутренних ресурсов (например, базы данных, файлы, API)
         resources: {},
+        // tools — описание инструментов/функций, которые сервер может выполнять (например, создание пользователя, отправка письма)
         tools: {},
+        // prompts — шаблоны или инструкции для генерации сообщений/запросов к модели (например, промпты для LLM)
         prompts: {},
     },
 });
@@ -25,22 +29,32 @@ server.registerTool(
             idempotentHint: false, // повторный вызов с теми же параметрами может дать разный результат
             openWorldHint: true, // сервер может взаимодействовать с внешними системами
         },
-        inputSchema: {
-            name: z.string(),
-            email: z.string(),
-            address: z.string(),
-            phoneNumber: z.string(),
-        },
+        inputSchema: UserSchema.shape,
     },
-    ({ name, email, address, phoneNumber }) => {
-        return {
-            content: [
-                {
-                    type: 'text',
-                    text: `User ${name} created successfully`,
-                },
-            ],
-        };
+    async ({ name, email, address, phoneNumber }) => {
+        try {
+            const id = await createUser({ name, email, address, phoneNumber });
+
+            console.log(id);
+
+            return {
+                content: [
+                    {
+                        type: 'text',
+                        text: `User ${name} with id: ${id} created successfully`,
+                    },
+                ],
+            };
+        } catch (error) {
+            return {
+                content: [
+                    {
+                        type: 'text',
+                        text: `Error creating user: ${error}`,
+                    },
+                ],
+            };
+        }
     },
 );
 
