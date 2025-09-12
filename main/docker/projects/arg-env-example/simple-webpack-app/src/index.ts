749ac3c78afeb74e0e34a347e6b63b3ce82e9a6d
maxim1006 <maximprosv@gmail.com>
2025-09-12 17:51:07 +0300

    docs(TICKET-01157): updated readme

--- main/docker/projects/arg-env-example/simple-webpack-app/src/index.ts
+++ main/docker/projects/arg-env-example/simple-webpack-app/src/index.ts
@@ -1,4 +1,12 @@
-__webpack_public_path__ = __VARIABLE_FROM_DOCKER_ARG__;
+__webpack_public_path__ = __VARIABLE_FROM_DOCKER_ARG__; // __webpack_public_path__ === my arg
 
-console.log('__webpack_public_path__ ', __webpack_public_path__);
-console.log(`__VARIABLE_FROM_DOCKER_ENV__: ${__VARIABLE_FROM_DOCKER_ENV__}`);
+console.log('__webpack_public_path__ ', __webpack_public_path__); // my arg
+console.log(`__VARIABLE_FROM_DOCKER_ENV__: ${__VARIABLE_FROM_DOCKER_ENV__}`); // custom env
+
+window.document.addEventListener('click', async (): Promise<any> => {
+    // вместо стандартного пути будет /my%20arg778.index.js из-за __webpack_public_path__
+    const { default: export1 } = await import('./scripts/export1');
+    const { default: export2 } = await import('./scripts/export2');
+
+    console.log(export1, export2);
+});
---
+++ main/docker/projects/arg-env-example/simple-webpack-app/src/scripts/export1.ts
@@ -0,0 +1,4 @@
+export default 'export1';
+
+// для проверки соурс мап
+throw new Error('some error');
